52bd2461bb671c775bee017151c6ae3f4c9c460e
Kevin Huang <75445018+wrafp@users.noreply.github.com>
2021-12-13 22:49:03 +0000

    added variables in order for the a few of the drawings to scale with the dimensions of the image.

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,74 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  
   public void setup() {
-    background(210, 255, 173);
+    background(77, 27, 135);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
+    // Draws Moon
+    strokeWeight(3);
+    stroke(97, 48, 65);
+    fill(222, 177, 177);
+    stroke(222, 177, 177);
+    ellipse(width / 1.18f, height / 10, width / 10, width / 10);
+    fill(77, 27, 135);
+    stroke(77, 27, 135);
+    ellipse(width * 0.89f, height / 10, width / 10, width / 10);
+    
+    // Draws Buildings (Back Layer)
+    strokeWeight(3);
+    stroke(45);
+    fill(60);
+    rect(0, 120, 85, 300);
+    rect(85, 155, 80, 300);
+    rect(165, 140, 80, 300);
+    rect(310, 170, 70, 300);
+    rect(220, 190, 100, 300);
+    rect(360, 135, 80, 300);
+    rect(430, 125, 80, 300);
+    
+    // Draws Buildings (Front Layer)
+    stroke(50);
+    fill(70);
+    rect(0, 210, 80, 300);
+    stroke(48);
+    fill(65);
+    rect(65, 180, 105, 340);
+    rect(250, 235, 85, 300);
+    stroke(50);
+    fill(70);
+    rect(width * 0.32f, height * 0.6f, 140, 340);
+    triangle(158, 300, 230, 250, 302, 300);
+    rect(320, 255, 100, 300);
+    rect(385, 215, 80, 300);
+    rect(440, 285, 80, 300);
+    
+    // Draws Stars
     stroke(255);
-    line(50, 125, 70, 50);  
+    point(20, 60);
+    point(50, 30);
+    point(110, 120);
+    point(80, 90);
+    point(120, 50);
+    point(190, 130);
+    point(230, 80);
+    point(240, 30);
+    point(200, 20);
+    point(280, 150);
+    point(320, 30);
+    point(330, 100);
+    point(360, 60);
+    point(450, 110);
+    point(480, 20);
   }
-  
-  // define other methods down here.
 }

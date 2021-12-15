ac6a122f00c8333422df2104df82ac238eb155cc
Kevin Huang <75445018+wrafp@users.noreply.github.com>
2021-12-15 18:25:27 +0000

    added variables for position for all the buildings

--- Sketch.java
+++ Sketch.java
@@ -1,22 +1,21 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+  public int intSizeX = 500;
+  public int intSizeY = 300;
 
   public void settings() {
-	// put your size call here
-    size(500, 500);
+    size(intSizeX, intSizeY);
   }
 
-  
   public void setup() {
-    background(77, 27, 135);
-  }
+      background(77, 27, 135);
+  } 
 
-  
   public void draw() {
-	  
+
     // Draws Moon
+
     strokeWeight(3);
     stroke(97, 48, 65);
     fill(222, 177, 177);
@@ -30,29 +29,29 @@ public class Sketch extends PApplet {
     strokeWeight(3);
     stroke(45);
     fill(60);
-    rect(0, 120, 85, 300);
-    rect(85, 155, 80, 300);
-    rect(165, 140, 80, 300);
-    rect(310, 170, 70, 300);
-    rect(220, 190, 100, 300);
-    rect(360, 135, 80, 300);
-    rect(430, 125, 80, 300);
+    rect(0, intSizeX / 4.17f, intSizeX / 5.88f, intSizeY / 1.67f);
+    rect(intSizeX / 5.88f, intSizeY / 3.23f, intSizeX / 6.25f, intSizeY / 1.67f);
+    rect(intSizeX / 3.03f, intSizeY / 3.57f, intSizeX / 6.25f, intSizeY / 1.67f);
+    rect(intSizeX / 1.61f, intSizeY / 2.94f, intSizeX / 7.14f, intSizeY / 1.67f);
+    rect(intSizeX / 2.27f, intSizeY / 2.63f, intSizeX / 5, intSizeY / 1.67f);
+    rect(intSizeX / 1.39f, intSizeY / 3.7f, intSizeX / 6.25f, intSizeY / 1.67f);
+    rect(intSizeX / 1.16f, intSizeY / 4, intSizeX / 6.25f, intSizeY / 1.67f);
     
     // Draws Buildings (Front Layer)
     stroke(50);
     fill(70);
-    rect(0, 210, 80, 300);
+    rect(0, intSizeY / 2.38f, intSizeX / 6.25f, intSizeY / 1.67f);
     stroke(48);
     fill(65);
-    rect(65, 180, 105, 340);
-    rect(250, 235, 85, 300);
+    rect(intSizeX / 7.69f, intSizeY / 2.78f, intSizeX / 4.76f, intSizeY / 1.4f);
+    rect(intSizeX / 2, intSizeY / 2.13f, intSizeX / 5.88f, intSizeY / 1.67f);
     stroke(50);
     fill(70);
-    rect(width * 0.32f, height * 0.6f, 140, 340);
-    triangle(158, 300, 230, 250, 302, 300);
-    rect(320, 255, 100, 300);
-    rect(385, 215, 80, 300);
-    rect(440, 285, 80, 300);
+    rect(width * 0.32f, height * 0.6f, 140, intSizeY / 1.4f);
+    triangle(intSizeX / 3.16f, intSizeY / 1.67f, intSizeX / 2.17f, intSizeY / 2, intSizeX / 1.66f, intSizeY / 1.67f);
+    rect(intSizeX / 1.56f, intSizeY / 1.96f, intSizeX / 5, intSizeY / 1.67f);
+    rect(intSizeX / 1.3f, intSizeY / 2.33f, intSizeX / 6.25f, intSizeY / 1.67f);
+    rect(intSizeX / 1.14f, intSizeY / 1.75f, intSizeX / 6.25f, intSizeY / 1.67f);
     
     // Draws Stars
     stroke(255);
@@ -73,3 +72,5 @@ public class Sketch extends PApplet {
     point(480, 20);
   }
 }
+  
+
